356817970b1e5baef6303662457217893289f2ce
James <james@notjam.es>
2014-07-13 02:32:46 -0400

    use a less insane format for the timezone data

--- ballparkTimezone.js
+++ ballparkTimezone.js
@@ -6,81 +6,81 @@
 (function(Date) {
 	//  "OFFSET:DST_AMOUNT:DST_START": "TIMEZONE_ID
 	var timezones = {
-		"-39600:0:0":"Pacific/Midway",
-		"-36000:3600:1331467200":"America/Adak",
-		"-36000:0:0":"Pacific/Honolulu",
-		"-34200:0:0":"Pacific/Marquesas",
-		"-32400:3600:1331463600":"America/Juneau",
-		"-32400:0:0":"Pacific/Gambier",
-		"-28800:3600:1333274400":"America/Santa_Isabel",
-		"-28800:3600:1331460000":"America/Los_Angeles",
-		"-28800:0:0":"America/Metlakatla",
-		"-25200:3600:1333270800":"America/Chihuahua",
-		"-25200:3600:1331456400":"America/Denver",
-		"-25200:0:0":"America/Phoenix",
-		"-21600:3600:1333267200":"America/Mexico_City",
-		"-21600:3600:1331452800":"America/Chicago",
-		"-21600:0:0":"America/Guatemala",
-		"-18000:3600:1331449200":"America/New_York",
-		"-18000:3600:1331442000":"America/Havana",
-		"-18000:0:0":"America/Cayman",
-		"-18000:-3600:1331434800":"Pacific/Easter",
-		"-16200:0:0":"America/Caracas",
-		"-14400:3600:1331445600":"Atlantic/Bermuda",
-		"-14400:0:0":"America/Puerto_Rico",
-		"-12600:3600:1331443800":"America/St_Johns",
-		"-10800:3600:1332637200":"America/Godthab",
-		"-10800:3600:1331442000":"America/Miquelon",
-		"-10800:0:0":"America/Fortaleza",
-		"-10800:-3600:1334466000":"Atlantic/Stanley",
-		"-10800:-3600:1333854000":"America/Asuncion",
-		"-10800:-3600:1331434800":"America/Santiago",
-		"-10800:-3600:1330225200":"America/Campo_Grande",
-		"-7200:0:0":"America/Noronha",
-		"-7200:-3600:1331438400":"America/Montevideo",
-		"-7200:-3600:1330221600":"America/Bahia",
-		"-3600:3600:1332637200":"America/Scoresbysund",
-		"-3600:0:0":"Atlantic/Cape_Verde",
-		"0:0:0":"UTC",
-		"0:3600:1332637200":"Europe/London",
-		"3600:0:0":"Africa/Algiers",
-		"3600:3600:1332637200":"Europe/Stockholm",
-		"7200:-3600:1333238400":"Africa/Windhoek",
-		"7200:0:0":"Africa/Cairo",
-		"7200:3600:1332626400":"Asia/Beirut",
-		"7200:3600:1332637200":"Europe/Kiev",
-		"7200:3600:1333058400":"Asia/Amman",
-		"7200:3600:1333065600":"Asia/Jerusalem",
-		"7200:3600:1333663200":"Asia/Damascus",
-		"10800:0:0":"Africa/Nairobi",
-		"12600:3600:1332275400":"Asia/Tehran",
-		"14400:0:0":"Asia/Dubai",
-		"14400:3600:1332626400":"Asia/Yerevan",
-		"14400:3600:1332633600":"Asia/Baku",
-		"16200:0:0":"Asia/Kabul",
-		"18000:0:0":"Asia/Karachi",
-		"19800:0:0":"Asia/Colombo",
-		"20700:0:0":"Asia/Kathmandu",
-		"21600:0:0":"Asia/Dhaka",
-		"23400:0:0":"Asia/Rangoon",
-		"25200:0:0":"Asia/Bangkok",
-		"28800:0:0":"Asia/Hong_Kong",
-		"31500:0:0":"Australia/Eucla",
-		"32400:0:0":"Asia/Tokyo",
-		"34200:0:0":"Australia/Darwin",
-		"36000:0:0":"Australia/Brisbane",
-		"37800:-3600:1333211400":"Australia/Broken_Hill",
-		"39600:-1800:1333206000":"Australia/Lord_Howe",
-		"39600:-3600:1333209600":"Australia/Sydney",
-		"39600:0:0":"Pacific/Pohnpei",
-		"41400:0:0":"Pacific/Norfolk",
-		"43200:0:0":"Asia/Anadyr",
-		"46800:-3600:1327154400":"Pacific/Fiji",
-		"46800:-3600:1333202400":"Antarctica/South_Pole",
-		"46800:0:0":"Pacific/Enderbury",
-		"49500:-3600:1333202400":"Pacific/Chatham",
-		"50400:-3600:1333202400":"Pacific/Apia",
-		"50400:0:0":"Pacific/Kiritimati",
+		"Pacific/Midway": { offset: -39600, savings: 0, savingsStart: 0 },
+		"America/Adak": { offset: -36000, savings: 3600, savingsStart: 1331467200 },
+		"Pacific/Honolulu": { offset: -36000, savings: 0, savingsStart: 0 },
+		"Pacific/Marquesas": { offset: -34200, savings: 0, savingsStart: 0 },
+		"America/Juneau": { offset: -32400, savings: 3600, savingsStart: 1331463600 },
+		"Pacific/Gambier": { offset: -32400, savings: 0, savingsStart: 0 },
+		"America/Santa_Isabel": { offset: -28800, savings: 3600, savingsStart: 1333274400 },
+		"America/Los_Angeles": { offset: -28800, savings: 3600, savingsStart: 1331460000 },
+		"America/Metlakatla": { offset: -28800, savings: 0, savingsStart: 0 },
+		"America/Chihuahua": { offset: -25200, savings: 3600, savingsStart: 1333270800 },
+		"America/Denver": { offset: -25200, savings: 3600, savingsStart: 1331456400 },
+		"America/Phoenix": { offset: -25200, savings: 0, savingsStart: 0 },
+		"America/Mexico_City": { offset: -21600, savings: 3600, savingsStart: 1333267200 },
+		"America/Chicago": { offset: -21600, savings: 3600, savingsStart: 1331452800 },
+		"America/Guatemala": { offset: -21600, savings: 0, savingsStart: 0 },
+		"America/New_York": { offset: -18000, savings: 3600, savingsStart: 1331449200 },
+		"America/Havana": { offset: -18000, savings: 3600, savingsStart: 1331442000 },
+		"America/Cayman": { offset: -18000, savings: 0, savingsStart: 0 },
+		"Pacific/Easter": { offset: -18000, savings: -3600, savingsStart: 1331434800 },
+		"America/Caracas": { offset: -16200, savings: 0, savingsStart: 0 },
+		"Atlantic/Bermuda": { offset: -14400, savings: 3600, savingsStart: 1331445600 },
+		"America/Puerto_Rico": { offset: -14400, savings: 0, savingsStart: 0 },
+		"America/St_Johns": { offset: -12600, savings: 3600, savingsStart: 1331443800 },
+		"America/Godthab": { offset: -10800, savings: 3600, savingsStart: 1332637200 },
+		"America/Miquelon": { offset: -10800, savings: 3600, savingsStart: 1331442000 },
+		"America/Fortaleza": { offset: -10800, savings: 0, savingsStart: 0 },
+		"Atlantic/Stanley": { offset: -10800, savings: -3600, savingsStart: 1334466000 },
+		"America/Asuncion": { offset: -10800, savings: -3600, savingsStart: 1333854000 },
+		"America/Santiago": { offset: -10800, savings: -3600, savingsStart: 1331434800 },
+		"America/Campo_Grande": { offset: -10800, savings: -3600, savingsStart: 1330225200 },
+		"America/Noronha": { offset: -7200, savings: 0, savingsStart: 0 },
+		"America/Montevideo": { offset: -7200, savings: -3600, savingsStart: 1331438400 },
+		"America/Bahia": { offset: -7200, savings: -3600, savingsStart: 1330221600 },
+		"America/Scoresbysund": { offset: -3600, savings: 3600, savingsStart: 1332637200 },
+		"Atlantic/Cape_Verde": { offset: -3600, savings: 0, savingsStart: 0 },
+		"UTC": { offset: 0, savings: 0, savingsStart: 0 },
+		"Europe/London": { offset: 0, savings: 3600, savingsStart: 1332637200 },
+		"Africa/Algiers": { offset: 3600, savings: 0, savingsStart: 0 },
+		"Europe/Stockholm": { offset: 3600, savings: 3600, savingsStart: 1332637200 },
+		"Africa/Windhoek": { offset: 7200, savings: -3600, savingsStart: 1333238400 },
+		"Africa/Cairo": { offset: 7200, savings: 0, savingsStart: 0 },
+		"Asia/Beirut": { offset: 7200, savings: 3600, savingsStart: 1332626400 },
+		"Europe/Kiev": { offset: 7200, savings: 3600, savingsStart: 1332637200 },
+		"Asia/Amman": { offset: 7200, savings: 3600, savingsStart: 1333058400 },
+		"Asia/Jerusalem": { offset: 7200, savings: 3600, savingsStart: 1333065600 },
+		"Asia/Damascus": { offset: 7200, savings: 3600, savingsStart: 1333663200 },
+		"Africa/Nairobi": { offset: 10800, savings: 0, savingsStart: 0 },
+		"Asia/Tehran": { offset: 12600, savings: 3600, savingsStart: 1332275400 },
+		"Asia/Dubai": { offset: 14400, savings: 0, savingsStart: 0 },
+		"Asia/Yerevan": { offset: 14400, savings: 3600, savingsStart: 1332626400 },
+		"Asia/Baku": { offset: 14400, savings: 3600, savingsStart: 1332633600 },
+		"Asia/Kabul": { offset: 16200, savings: 0, savingsStart: 0 },
+		"Asia/Karachi": { offset: 18000, savings: 0, savingsStart: 0 },
+		"Asia/Colombo": { offset: 19800, savings: 0, savingsStart: 0 },
+		"Asia/Kathmandu": { offset: 20700, savings: 0, savingsStart: 0 },
+		"Asia/Dhaka": { offset: 21600, savings: 0, savingsStart: 0 },
+		"Asia/Rangoon": { offset: 23400, savings: 0, savingsStart: 0 },
+		"Asia/Bangkok": { offset: 25200, savings: 0, savingsStart: 0 },
+		"Asia/Hong_Kong": { offset: 28800, savings: 0, savingsStart: 0 },
+		"Australia/Eucla": { offset: 31500, savings: 0, savingsStart: 0 },
+		"Asia/Tokyo": { offset: 32400, savings: 0, savingsStart: 0 },
+		"Australia/Darwin": { offset: 34200, savings: 0, savingsStart: 0 },
+		"Australia/Brisbane": { offset: 36000, savings: 0, savingsStart: 0 },
+		"Australia/Broken_Hill": { offset: 37800, savings: -3600, savingsStart: 1333211400 },
+		"Australia/Lord_Howe": { offset: 39600, savings: -1800, savingsStart: 1333206000 },
+		"Australia/Sydney": { offset: 39600, savings: -3600, savingsStart: 1333209600 },
+		"Pacific/Pohnpei": { offset: 39600, savings: 0, savingsStart: 0 },
+		"Pacific/Norfolk": { offset: 41400, savings: 0, savingsStart: 0 },
+		"Asia/Anadyr": { offset: 43200, savings: 0, savingsStart: 0 },
+		"Pacific/Fiji": { offset: 46800, savings: -3600, savingsStart: 1327154400 },
+		"Antarctica/South_Pole": { offset: 46800, savings: -3600, savingsStart: 1333202400 },
+		"Pacific/Enderbury": { offset: 46800, savings: 0, savingsStart: 0 },
+		"Pacific/Chatham": { offset: 49500, savings: -3600, savingsStart: 1333202400 },
+		"Pacific/Apia": { offset: 50400, savings: -3600, savingsStart: 1333202400 },
+		"Pacific/Kiritimati": { offset: 50400, savings: 0, savingsStart: 0 },
 	};
 
 	var getTimezoneOffset = function(timestamp) {
@@ -95,25 +95,28 @@
 		var januaryTimezoneOffset = getTimezoneOffset(1388552400);
 		var juneTimezoneOffset = getTimezoneOffset(1401595200);
 
-		var savingsOffset = juneTimezoneOffset - januaryTimezoneOffset;
+		var savings = juneTimezoneOffset - januaryTimezoneOffset;
 
-		var tzKey = januaryTimezoneOffset + ':' + savingsOffset;
-
-		for (var a in timezones) {
-			if (timezones.hasOwnProperty(a)) {
-				if (a.indexOf(tzKey) != 0)
+		for (var name in timezones) {
+			if (timezones.hasOwnProperty(name)) {
+				if (timezones[name].offset !== januaryTimezoneOffset) {
 					continue;
+				}
 
-				var tzInfo = a.split(':');
+				if (timezones[name].savings !== savings) {
+					continue;
+				}
 
-				if (tzInfo[1] == 0)
-					return timezones[a];
+				if (timezones[name] === 0) {
+					return name;
+				}
 
-				var beforeOffset = getTimezoneOffset(parseInt(tzInfo[2]) - 1);
-				var afterOffset = getTimezoneOffset(parseInt(tzInfo[2]));
+				var beforeOffset = getTimezoneOffset(timezones[name].savingsStart - 1);
+				var afterOffset = getTimezoneOffset(timezones[name].savingsStart);
 
-				if (beforeOffset !== afterOffset)
-					return timezones[a];
+				if (beforeOffset !== afterOffset) {
+					return name;
+				}
 			}
 		}
 
